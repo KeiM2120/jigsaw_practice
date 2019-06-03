7384a803945b2217f025af69f081f8712aa9e946
KeiM2120 <kei.mmpublic@gmail.com>
2019-06-03 12:54:29 +0900

    index.blade.php 追記

--- source/index.blade.php
+++ source/index.blade.php
@@ -9,5 +9,13 @@ But I don't know how to make HTML/CSS
 
 <div class="class1">This is Class 1 area . </div>
 <div class="class2"><style>div.class2{background: blue;}</style>This is Class 2 area . </div>
+<div class="introduce">
+    LIKE
+    <ul>
+        <li>ハンバーガー</li>
+        <li>ゲーム(多種)</li>
+        <li>音楽(SoundCloudを利用)</li>
+    </ul>
+</div>
 
 @endsection
