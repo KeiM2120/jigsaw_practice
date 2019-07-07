b2728669ba2b04a3a4ca690dfab3ad925dacc317
KeiM2120 <kei.mmpublic@gmail.com>
2019-07-07 21:03:22 +0900

    作り直しとCSSの読み込み方の確認

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,38 +2,17 @@
 @extends('_layouts.header')
 
 
+@section('bodyStyle')
+<link rel="stylesheet" href="assets/build/css/index.css">
+@endsection
+
 @section('body')
 
-<h1>Hello!! This is my portforio</h1>
-
-<p>Using blade templates.</p>
-But I don't know how to make HTML/CSS
-
-<div class="class1">This is Class 1 area . </div>
-<div class="class2">This is Class 2 area . </div>
-<div class="introduce">
-    <div class="likes">
-        <h4>LIKES</h4>
-        <ul>
-            <li>ハンバーガー</li>
-            <li>ゲーム(多種)</li>
-            <li>音楽(SoundCloudを利用)</li>
-        </ul>
-    </div>
-
-    <div class="skill">
-        <h4>SKILLS</h4>
-        <ul>
-            <li>java : 大学の授業にて利用 </li>
-            <li>PHP : ダイスロールアプリを作りたくて触り始める</li>
-            <li>Python : webスクレイプにて使用。AtCoderで利用中</li>
-        </ul>
-        <h4> LEARNING </h4>
-        <ul>
-            <li>Laravel : PHPおよびフレームワークの学習のため選択</li>
-            <li>Scala : 表現力、可読性、実行速度に惹かれて学び始める</li>
-        </ul>
-
-</div>
+<p align="center" class="main_title">
+    KEISUKE MAKITA 's
+    portforio site.
+</p>
 
 @endsection
+
+
